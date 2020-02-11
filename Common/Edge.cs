388b37165e3e01e8979307bed58c70b92aa2d652
Per G <pgg.kinna@gmail.com>
2020-02-11 13:44:59 +0100

    Add project files.

---
+++ Common/Edge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Edge
+    {
+        public double Cost { get; set; }
+        public Node Destination { get; set; }
+
+        public override string ToString()
+        {
+            return "-> " + Destination.ToString();
+        }
+    }
+}
---
+++ Common/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    class Graph
+    {
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public Node StartNode { get; set; }
+
+        public Node EndNode { get; set; }
+    }
+}
